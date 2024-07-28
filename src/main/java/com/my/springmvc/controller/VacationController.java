6cd3ed19cba0673b5ea15a790c25bff9d858f62e
myakk <myakk015@gmail.com>
2024-07-28 18:31:12 +0300

    28/07/2024

--- src/main/java/com/my/springmvc/controller/VacationController.java
+++ src/main/java/com/my/springmvc/controller/VacationController.java
@@ -2,6 +2,7 @@ package com.my.springmvc.controller;
 
 import com.my.springmvc.dao.VacationDAO;
 import com.my.springmvc.model.Person;
+import com.my.springmvc.model.Vacation;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,60 +13,21 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/vacations")
 public class VacationController {
-    /*private final VacationDAO vacationDAO;
+/*    private final VacationDAO vacationDAO;
 
     @Autowired
     public VacationController(VacationDAO vacationDAO) {
         this.vacationDAO = vacationDAO;
     }
 
-    @GetMapping
-    public String index(Model model) {
-        model.addAttribute("vacations", vacationDAO.index());
-        return "vacations/index";
-    }
-
-    @GetMapping("/new")
-    public String newPerson(@ModelAttribute("vacation'/'") Person person) {
-        return "vacation/new";
-    }
-
     @PostMapping()
-    public String create(@ModelAttribute("vacation") @Valid Person person, BindingResult bindingResult) {
+    public String create(@ModelAttribute("vacation") @Valid Vacation vacation, BindingResult bindingResult) {
         if(bindingResult.hasErrors()) {
-            return "vacations/new";
+            return "persons/new";
         }
 
-        vacationDAO.save(person);
-        return "redirect:/vacations";
-    }
-
-    @GetMapping("/{id}")
-    public String show(@PathVariable("id") int id, Model model) {
-        model.addAttribute("vacation", vacationDAO.show(id));
-        return "vacations/show";
-    }
-
-    @GetMapping("/{id}/edit")
-    public String edit(Model model, @PathVariable("id") int id) {
-        model.addAttribute("vacation", vacationDAO.show(id));
-        return "vacations/edit";
-    }
-
-    @PatchMapping("/{id}")
-    public String update(@ModelAttribute("vacation") @Valid Person person, BindingResult bindingResult,
-                         @PathVariable("id") int id) {
-        if(bindingResult.hasErrors()) {
-            return "vacations/edit";
-        }
-
-        vacationDAO.update(id, person);
-        return "redirect:/vacations";
-    }
-
-    @DeleteMapping("/id")
-    public String delete(@PathVariable("id") int id) {
-        vacationDAO.delete(id);
-        return "redirect:/vacations";
+        personDAO.save(person);
+        return "redirect:/persons";
     }*/
+
 }
